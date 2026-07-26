6b5df047e98a6d2c97aea92e4000ff45e97b2bbb
Mauricio Gonzalez <mauriciogonzalezdeveloper@gmail.com>
2026-07-26 12:18:00 -0500

    Add Helmet security middleware for HTTP headers protection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 import 'dotenv/config';
 import routes from './routes/index';
 import { errorHandler } from './middlewares/errorHandler';
@@ -7,6 +8,7 @@ import { errorHandler } from './middlewares/errorHandler';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
